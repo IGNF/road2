45b377d2ea2d03f01faadb0c59c415107fbd655c
Loic Grondin <loic.grondin@ign.fr>
2018-12-14 14:29:43 +0100

    Integration d'OSRM avec le binding nodejs sur debian

--- src/server.js
+++ src/server.js
@@ -1,15 +1,22 @@
 'use strict';
 
 const express = require('express');
+const OSRM = require("osrm");
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
+const osrm = new OSRM("/home/docker/data/corse-latest.osrm");
 
 // App
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello world !! \n');
+  osrm.route({coordinates: [[ 9.479455,42.546907], [9.435853,42.619204]]}, function(err, result) {
+    if(err) throw err;
+    console.log(result.waypoints); // array of Waypoint objects representing all waypoints in order
+    console.log(result.routes); // array of Route objects ordered by descending recommendation rank
+  });
 });
 
 app.listen(PORT, HOST);
