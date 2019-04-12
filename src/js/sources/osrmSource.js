2dc8e0d46da3a0fe355eac40c20428059abeb10c
Loic Grondin <loic.grondin@ign.fr>
2019-04-12 12:13:56 +0200

    ajout d'un controler pour l'api, tests du controler

--- src/js/sources/osrmSource.js
+++ src/js/sources/osrmSource.js
@@ -173,7 +173,6 @@ module.exports = class osrmSource extends Source {
     return new Promise ( (resolve, reject) => {
       this.osrm.route(osrmRequest, (err, result) => {
         if (err) {
-          console.log(err);
           reject(err);
         } else {
           resolve(this.writeRouteResponse(request, result));
