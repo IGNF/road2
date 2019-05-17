34269c1c00187551180759d1b0ff9fb2e664024c
azarzelli <amaury.zarzelli@ign.fr>
2019-05-17 18:02:40 +0200

    More mocks!! Mais un unit test resiste encore et toujours à l'envahiseur...

--- test/mocha/service/testsService.js
+++ test/mocha/service/testsService.js
@@ -1,9 +1,14 @@
 const assert = require('assert');
 const Service = require('../../../src/js/service/service');
 const RouteRequest = require('../../../src/js/requests/routeRequest');
+const SourceManager = require('../../../src/js/sources/sourceManager');
+const Resource = require('../../../src/js/resources/resource');
+const Source = require('../../../src/js/sources/source');
 const logManager = require('../logManager');
 const path = require('path');
 const fs = require('fs');
+
+const sinon = require('sinon');
 
 describe('Test de la classe Service', function() {
 
@@ -44,6 +49,15 @@ describe('Test de la classe Service', function() {
   describe('Test de loadSources()', function() {
 
     it('loadSources() return true avec une configuration correcte', async function() {
+      const sourceManager = sinon.mock(SourceManager);
+      // Mocking the source manager
+      sourceManager.listOfSourceIds = ["toto"];
+      sourceManager.sourceDescriptions = {"toto": 0};
+      sourceManager.createSource = sinon.stub().returns(sinon.mock(Source));
+      sourceManager.connectSource = sinon.stub().returns(true);
+      sourceManager.disconnectSource = sinon.stub().returns(true);
+      service._sourceManager = sourceManager;
+
       const sourcesLoaded = await service.loadSources();
       assert.equal(sourcesLoaded, true);
       await service.disconnectAllSources();
@@ -71,6 +85,21 @@ describe('Test de la classe Service', function() {
 
     it('computeRequest() avec une requete correcte', async function() {
       const request = new RouteRequest("corse-osm", {lon: 8.732901, lat: 41.928821}, {lon: 8.732901, lat: 41.953932}, "car", "fastest");
+
+      const resource = sinon.mock(Resource);
+      resource.getSourceIdFromRequest = sinon.stub().returns("sourcetest");
+
+      const fakeRouteResponse = {"resource":"corse-osm","start":"8.732901,41.928821","end":"8.763831,41.953897","profile":"car","optimization":"fastest","geometry":"cf|~Fssht@tAgLFiNqJTaEuFiEpEgIxCsLdDwDw@oIac@mAg@cKkTBiBeAaByCqSkHc[mGaQHiGgB_@wL{[_FvBsDmEuEeB{RjGk@e@Z{B","portions":[{"start":"8.732901,41.928821","end":"8.763831,41.953897","steps":[{"geometry":"cf|~Fssht@B@"},{"geometry":"_f|~Fqsht@Rw@Hq@HwBL_ALw@LoAIuCG{BA]AY@SZkC"},{"geometry":"ec|~Fepit@MC_@Gg@LUFm@PwEEe@Qc@Y"},{"geometry":"aq|~F{pit@CIGOYk@g@aAi@aA"},{"geometry":"yt|~Fewit@SRkAtAa@b@gAbAMF_APs@JQGI@aA\\o@d@Y^]Tc@HuBJ"},{"geometry":"ej}~Fckit@e@PKDIB}Aj@_CfAu@Fu@@e@Q"},{"geometry":"qw}~F{fit@?KAKCIEGGEGAI@"},{"geometry":"wx}~Fkhit@e@aDm@kDs@uDc@_COq@_BgGm@cBOe@"},{"geometry":"qc~~Fsjjt@JS@SCQCKGEMCK?OH"},{"geometry":"}d~~Fyljt@W[[u@uAiCm@gAcDcH[o@Uq@M]Mk@"},{"geometry":"gr~~Fmdkt@HKFM@QAQGMIIKEMB"},{"geometry":"as~~Fegkt@YgAG[WsAKk@Ms@_@qBs@yDG]SgA"},{"geometry":"ey~~Fo|kt@JIDWAMIOSE"},{"geometry":"sy~~Fu~kt@_AaGYkA_C}I_@iAW_A"},{"geometry":"eb__Gkxlt@FMBOCMCKGECCO@"},{"geometry":"}b__Gkzlt@oC}Hg@iBy@qCG_AEm@C}@"},{"geometry":"ak__Gqqmt@LGJKDOBQ?IAKEQGKMIMCK@KDIHEL"},{"geometry":"ql__Gitmt@[e@Uu@s@_D{@oCiB_CkBmE{@iCM_@Q}@"},{"geometry":"e{__Gkrnt@u@h@g@\\k@\\m@Pg@?aAe@i@cAgAcB{Ac@yBaA"},{"geometry":"on`_Ggxnt@iBXyA`@{@`@c@RkAn@s@VaCf@uAh@"},{"geometry":"kba_G{ont@QIQIGQA]DUH[Lk@"},{"geometry":"{ba_G}tnt@"}]}]};
+
+      const fakeSource = sinon.mock(Source);
+      fakeSource.computeRequest = sinon.stub().returns(fakeRouteResponse);
+
+      const fakeSourceCatalog = {"sourcetest": fakeSource};
+      service._sourceCatalog = fakeSourceCatalog;
+
+      const fakeResourceCatalog = {"corse-osm": resource};
+      service._resourceCatalog = fakeResourceCatalog;
+
       const response = await service.computeRequest(request);
       assert.equal(response.resource, "corse-osm");
     });
