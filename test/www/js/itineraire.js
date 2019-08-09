a3c4270074816e141cc76306fcefb2cc0b0c5ff3
Loic Grondin <loic.grondin@ign.fr>
2019-08-09 10:40:26 +0200

    Ajout de la page web de test dans le docker-compose et enrichissement de la page

--- test/www/js/itineraire.js
+++ test/www/js/itineraire.js
@@ -1,16 +1,22 @@
 //Variables Globales
 
-var road2Url = 'http://localhost:8080/simple/1.0.0/route?resource=corse-osm'
-
-
-//Carte
+var road2Url = "http://localhost:8080/simple/1.0.0/route?";
 var map;
 var clickedPoints = new Array();
-var vectorSource = new ol.source.Vector(),
-    vectorLayer = new ol.layer.Vector({
+
+// Variables par défaut que l'utilisateur peut modifier
+var defaultResource="corse-osm";
+var defaultProfile="car";
+var defaultOptimization="fastest";
+
+//Carte
+var vectorSource = new ol.source.Vector();
+
+var vectorLayer = new ol.layer.Vector({
       source: vectorSource
-    }),
-    styles = {
+    });
+
+var styles = {
       route: new ol.style.Style({
         stroke: new ol.style.Stroke({
           width: 6, color: [40, 40, 40, 0.8]
@@ -94,33 +100,135 @@ function createMap() {
 
       // on stocke le point
       clickedPoints.push(clickedCoordinate);
+
+      // en fonction du nombre de points stockés, on adapte le comportement
+      if (clickedPoints.length === 1) {
+        // on met les coordonnées dans le formulaire (start)
+        var input = document.getElementById('userStart');
+        input.value = clickedCoordinate;
+      } else if (clickedPoints.length === 2) {
+        // on met les coordonnées dans le formulaire (end)
+        var input = document.getElementById('userEnd');
+        input.value = clickedCoordinate;
+      } else if (clickedPoints.length === 3) {
+        var currentInputEnd = document.getElementById('userEnd');
+        var currentInputInt = document.getElementById('userIntermediates');
+        currentInputInt.value = currentInputEnd.value;
+        currentInputEnd.value = clickedCoordinate;
+      } else if (clickedPoints.length > 3) {
+        var currentInputEnd = document.getElementById('userEnd');
+        var currentInputInt = document.getElementById('userIntermediates');
+        currentInputInt.value = currentInputInt.value + "|" + currentInputEnd.value;
+        currentInputEnd.value = clickedCoordinate;
+      } else {
+        return false;
+      }
+
+      
+
+      
       // on affiche ce point sur la carte
       utils.createFeature(clickedCoordinate);
 
-      if (clickedPoints.length < 2) {
-        return;
-      }
-
-      // on calcule l'itinéraire
-      fetch(road2Url + '&start=' + clickedPoints[clickedPoints.length-2] + "&end="  + clickedPoints[clickedPoints.length-1] + "&geometryFormat=polyline")
-      .then(function(r) {
-        return r.json();
-      })
-      .then(function(json) {
-        utils.createRoute(json.geometry);
-      });
+      return true;
 
     });
 
 
 }
 
+
+// Calculer un itinéraire 
+function computeRoad() {
+
+  // console.log("Calcul d'un itinéraire");
+
+  // Déclarations 
+  var finalResource = "";
+  var finalOptimization = "";
+  var finalProfile = "";
+  var finalStart = "";
+  var finalEnd = "";
+  var finalIntermediates = "";
+
+  // Gestion des points utilisateur
+  if (clickedPoints.length < 2) {
+    // il n'y a pas assez de points pour faire un itinéraire 
+    return false; 
+  } else  {
+    finalStart = document.getElementById('userStart').value;
+    finalEnd = document.getElementById('userEnd').value;
+    finalIntermediates = document.getElementById('userIntermediates').value;
+  }
+
+
+  // Gestion des paramètres de l'utilisateur 
+
+  if (document.forms["route-form"].elements["userResource"].value !== "") {
+    finalResource = document.forms["route-form"].elements["userResource"].value;
+  } else {
+    finalResource = defaultResource;
+  }
+
+  if (document.forms["route-form"].elements["userProfile"].value !== "") {
+    finalProfile = document.forms["route-form"].elements["userProfile"].value;
+  } else {
+    finalProfile = defaultProfile;
+  }
+
+  if (document.forms["route-form"].elements["userOptimization"].value !== "") {
+    finalOptimization = document.forms["route-form"].elements["userOptimization"].value;
+  } else {
+    finalOptimization = defaultOptimization;
+  }
+
+  // on calcule l'itinéraire
+  fetch(road2Url + 
+    "resource=" + finalResource + 
+    "&profile=" + finalProfile + 
+    "&optimization=" + finalOptimization +
+    "&start=" + finalStart + 
+    "&end="  + finalEnd + 
+    "&intermediates=" + finalIntermediates + 
+    "&geometryFormat=polyline&getGeometry=true&getBbox=true")
+  .then(function(r) {
+    return r.json();
+  })
+  .then(function(json) {
+    utils.createRoute(json.geometry);
+  });
+
+  return true;
+
+}
+
+// Supprimer l'itinéraire afficher sur la carte 
+function cancelRoad() {
+
+  // Nettoyage du vecteur qui contient les données sur la map 
+  vectorSource.clear();
+
+  // Nettoyage du tableau qui contient les points 
+  clickedPoints = new Array();
+
+  // Nettoyage du formulaire 
+  document.forms["route-form"].elements["userResource"].value = "";
+  document.forms["route-form"].elements["userProfile"].value = "";
+  document.forms["route-form"].elements["userOptimization"].value = "";
+  document.forms["route-form"].elements["userStart"].value = "";
+  document.forms["route-form"].elements["userEnd"].value = "";
+  document.forms["route-form"].elements["userIntermediates"].value = "";
+  
+}
+
 var utils = {
+
   to4326: function(coord) {
     return ol.proj.transform([
       parseFloat(coord[0]), parseFloat(coord[1])
     ], 'EPSG:3857', 'EPSG:4326');
   },
+
   createFeature: function(coord) {
     var feature = new ol.Feature({
       type: 'place',
@@ -129,6 +237,7 @@ var utils = {
     feature.setStyle(styles.icon);
     vectorSource.addFeature(feature);
   },
+
   createRoute: function(polyline) {
     var route = new ol.format.Polyline({
       factor: 1e5
@@ -143,4 +252,5 @@ var utils = {
     feature.setStyle(styles.route);
     vectorSource.addFeature(feature);
   }
+
 };
