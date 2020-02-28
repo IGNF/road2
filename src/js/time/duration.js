d39e9710d1759767ae3f1e96707cfde4fe81089c
loic.grondin@ign.fr <>
2020-02-28 09:34:54 +0100

    gestion du format standard pour duration

--- src/js/time/duration.js
+++ src/js/time/duration.js
@@ -17,7 +17,7 @@ module.exports = class Duration {
   * @function
   * @name constructor
   * @description Constructeur de la classe Duration
-  * @param {float} value - Valeur de la durée
+  * @param {float|string} value - Valeur de la durée
   * @param {string} unit - Unité de la durée
   *
   */
@@ -67,7 +67,7 @@ module.exports = class Duration {
       return false;
     }
 
-    if (unit !== "hour" && unit !== "minute" && unit !== "second") {
+    if (unit !== "hour" && unit !== "minute" && unit !== "second" && unit !== "standard") {
       return false;
     }
 
@@ -87,6 +87,16 @@ module.exports = class Duration {
       return true;
     }
 
+    if (unit === "hour" && this._unit === "standard") {
+      let array = this._value.split(":");
+      let h = parseFloat(array[0]);
+      let m = parseFloat(array[1]);
+      let s = parseFloat(array[2]);
+      this._value = h + m/60 + s/3600;
+      this._unit = "hour";
+      return true;
+    }
+
     if (unit === "minute" && this._unit === "hour") {
       this._value = this._value * 60;
       this._unit = "minute";
@@ -95,6 +105,16 @@ module.exports = class Duration {
 
     if (unit === "minute" && this._unit === "second") {
       this._value = this._value / 60;
+      this._unit = "minute";
+      return true;
+    }
+
+    if (unit === "minute" && this._unit === "standard") {
+      let array = this._value.split(":");
+      let h = parseFloat(array[0]);
+      let m = parseFloat(array[1]);
+      let s = parseFloat(array[2]);
+      this._value = h*60 + m + s/60;
       this._unit = "minute";
       return true;
     }
@@ -111,10 +131,58 @@ module.exports = class Duration {
       return true;
     }
 
+    if (unit === "second" && this._unit === "standard") {
+      let array = this._value.split(":");
+      let h = parseFloat(array[0]);
+      let m = parseFloat(array[1]);
+      let s = parseFloat(array[2]);
+      this._value = h*3600 + m*60 + s;
+      this._unit = "second";
+      return true;
+    }
+
+    if (unit === "standard" && this._unit === "second") {
+      this._value = this.convertSecondToStandard(this._value);
+      this._unit = "standard";
+      return true;
+    }
+
+    if (unit === "standard" && this._unit === "minute") {
+      this._value = this.convertSecondToStandard(this._value * 60);
+      this._unit = "standard";
+      return true;
+    }
+
+    if (unit === "standard" && this._unit === "hour") {
+      this._value = this.convertSecondToStandard(this._value * 3600);
+      this._unit = "standard";
+      return true;
+    }
+
     return false;
 
   }
 
+  /**
+  *
+  * @function
+  * @name convertSecondToStandard
+  * @description Convertir des secondes au format standard 
+  * @param{float} second - Nombre de secondes
+  *
+  */
+  convertSecondToStandard (sec) {
+
+    let s = sec % 60;
+    let mi = Math.floor(sec / 60);
+    let m = mi % 60;
+    let h = Math.floor(mi / 60);
+    if (s < 10) {s = "0" + s;}
+    if (m < 10) {m = "0" + m;}
+    if (h < 10) {h = "0" + h;}
+    return h + ":" + m + ":" + s;
+    
+  }
 
 
 }
