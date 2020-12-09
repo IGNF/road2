b23f93b82ba65277ae48cb9c97ce328896861619
Loic <loic.grondin@ign.fr>
2020-12-09 10:08:54 +0100

    test: ecriture de la nouvelle conf dans un espace temporaire

--- test/functional/configuration/cucumber/features/support/world.js
+++ test/functional/configuration/cucumber/features/support/world.js
@@ -1,6 +1,7 @@
 const { setWorldConstructor } = require("cucumber");
 const path = require('path');
 const fs = require('fs');
+const os = require('os');
 
 /**
 *
@@ -27,6 +28,9 @@ class road2World {
         // Emplacement du road2.js pour lancer le serveur
         this._road2 = "";
 
+        // Espace temporaire pour stocker les configurations de chaque test
+        this._tmpDirectory = os.tmpdir();
+
         // Contenu du server.json
         this._serverConf = {};
 
@@ -39,10 +43,13 @@ class road2World {
         // Contenu des ressources 
         this._resourceConf = {};
 
+        // Dossier temporaire pour le test en cours
+        this._tmpDirConf = "";
+
     }
 
     // Lecture de la configuration des tests 
-    loadConfiguration() {
+    loadTestConfiguration() {
 
         let configurationPath = path.resolve(__dirname, "../../configurations/local.json");
         let configuration = {};
@@ -52,6 +59,7 @@ class road2World {
             configuration = JSON.parse(fs.readFileSync(configurationPath));
             this._defaultServerConfiguration = configuration.defaultServerConfiguration;
             this._road2 = configuration.road2;
+            this._tmpDirectory = configuration.tmpDirectory;
 
             return true;
 
@@ -66,7 +74,20 @@ class road2World {
 
         let projDirFiles = new Array();
         let resourceDirFiles = new Array();
+        let newResourcesDirectories = new Array();
 
+        // 1. On créé l'espace temporaire pour la configuration du scénario en cours
+        if (!fs.existsSync(this._tmpDirectory)) {
+            fs.mkdirSync(this._tmpDirectory, {recursive: true, mode: "766"});
+        }
+
+        try {
+            this._tmpDirConf = fs.mkdtempSync(path.join(this._tmpDirectory, 'road2-'));
+        } catch(error) {
+            throw "Can't create tmp directory for this test: " + error;
+        }
+
+        // 2. On lit la configuration par défaut
         // Lecture du server.json
         try {
             this._serverConf = JSON.parse(fs.readFileSync(this._defaultServerConfiguration));
@@ -91,7 +112,7 @@ class road2World {
         }
 
         for (let i = 0; i < projDirFiles.length; i++) {
-            this._projConf[projDirFiles[i]] = JSON.parse(fs.readFileSync(projDirFiles[i]));
+            this._projConf[projDirFiles[i]] = JSON.parse(fs.readFileSync(path.join(projDir, projDirFiles[i])));
         }
 
         // Lecture des ressources 
@@ -100,19 +121,66 @@ class road2World {
         // Pour chaque dossier, on récupère l'ensemble des fichiers 
         for (let i = 0; i < resourceDir.length; i++) {
 
+            let directoryName = path.join(this._tmpDirConf, "resources-" + i);
+            newResourcesDirectories.push(directoryName);
+
+            if (!fs.existsSync(directoryName)) {
+                fs.mkdirSync(directoryName, {recursive: true, mode: "766"});
+            }
+
             try {
+
                 resourceDirFiles = fs.readdirSync(resourceDir[i]);
+
                 for (let j = 0; j < resourceDirFiles.length; j++) {
-                    this._resourceConf[resourceDir[i]] = {};
-                    this._resourceConf[resourceDir[i]][resourceDirFiles[j]] = JSON.parse(fs.readFileSync(resourceDirFiles[j]));
+                    this._resourceConf[directoryName] = {};
+                    this._resourceConf[directoryName][resourceDirFiles[j]] = JSON.parse(fs.readFileSync(path.join(resourceDir[i], resourceDirFiles[j])));
                 }
+
             } catch(error) {
                 return false;
             }
 
         }
-        
-        
+
+        // 3. On modifie la configuration pour qu'elle puisse être copiée dans l'espace temporaire 
+        // mais elle pourra de nouveau être modifiée dans la suite du scénario
+
+        // Emplacement du log4js.json
+        this._serverConf.application.logs.configuration = path.join(this._tmpDirConf, "log4js.json");
+
+        // Emplacement des ressources 
+        this._serverConf.application.resources.directories = newResourcesDirectories;
+
+        return true;
+
+    }
+
+    // Test de la configuration stockée 
+    testConfiguration() {
+
+        // On écrit la configuration dans l'espace temporaire
+        try {
+            fs.writeFileSync(path.join(this._tmpDirConf, "server.json"), JSON.stringify(this._serverConf));
+        } catch(error) {
+            throw "Can't write server.json : " + error;
+        }
+
+        try {
+            fs.writeFileSync(path.join(this._tmpDirConf, "log4js.json"), JSON.stringify(this._logConf));
+        } catch(error) {
+            throw "Can't write log4js.json : " + error;
+        }
+
+        Object.keys(this._resourceConf).forEach( resourceDir => {
+            Object.keys(this._resourceConf[resourceDir]).forEach( resourceFile => {
+                try {
+                    fs.writeFileSync(path.join(resourceDir, resourceFile), JSON.stringify(this._resourceConf[resourceDir][resourceFile]));
+                } catch(error) {
+                    throw "Can't write " + resourceFile + " in " + resourceDir + " : " + error;
+                }
+            });
+        });
         
     }
  
